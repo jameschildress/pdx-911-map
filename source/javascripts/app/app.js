38c0a4d94dc2fcb70e2e2dc723f8e9f5f3cfbe5c
James Childress <james.childress@me.com>
2013-12-06 00:19:05 -0800

    meow meow meow

---
+++ source/javascripts/app/app.js
@@ -0,0 +1,25 @@
+(function(){
+  'use strict';
+
+
+
+
+  window.App = {
+  
+    config: {  
+      dataURL:      'http://www.portlandonline.com/scripts/911incidents.cfm'
+    , refreshRate:  30000
+    , mapDivID:     'pdx911-map'
+    }
+  
+  , mapOptions: {
+      zoom:   6
+    , center: new google.maps.LatLng(45.5174, -122.6699)
+    }
+  
+  };
+
+
+
+
+}());
