3d7404d911b7fe1eb11d121fe4a245fb528b1a0f
James Childress <james.childress@me.com>
2013-12-10 08:21:55 -0800

    progress on timeAgoInWords()

---
+++ source/javascripts/app/time_ago.js
@@ -0,0 +1,42 @@
+(function(){
+  'use strict';
+
+
+
+  
+  var second = 1000
+    , minute = second * 60
+    , hour   = minute * 60
+
+  App.timeAgoInWords = function(date) {
+    
+    var now     = new Date()
+      , age     = now - date
+      , strings = []
+      , hours
+      , minutes
+      , seconds;
+      
+    hours   = Math.floor(age / hour);
+    minutes = Math.floor((age % hours) / minute);
+    
+    if (hours === 0 && minutes === 0) {
+      return "less than 1 minute ago";
+    }
+    
+    if (hours) {
+      strings.push(hours + (hours === 1 ? "hour" : "hours"));
+    }
+    if (minutes) {
+      strings.push(minutes + (minutes === 1 ? "minute" : "minutes"));
+    }
+    
+    return strings.join(' and ') + ' ago';
+    
+  };
+
+
+
+
+
+}());
