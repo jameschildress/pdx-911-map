5d4c96616d465a9cba3797ac13f5583475ecdab0
James Childress <james.childress@me.com>
2013-12-11 11:48:31 -0800

    alphabetized categories

--- source/javascripts/app/category.js
+++ source/javascripts/app/category.js
@@ -2,17 +2,18 @@
   'use strict';
 
   var p
-    , config = App.config;
+    , config = App.config
+    , categories = App.categories;
 
 
 
 
   App.Category = function($list, title) {
-    
+    categories.push(this);
     this.title = title;
-    this.$listGroup = $(this.listGroupHTML()).prependTo($list);
+    this.$listGroup = $(this.listGroupHTML());
+    this.renderIn($list);
     this.$itemsWrapper = this.$listGroup.find('.' + config.listItemsGroupClass);
-    
   };
   
   
@@ -21,17 +22,13 @@
   // If a category exists with the given title, return that title.
   // Otherwise, return a new category and append it to the categories array.
   App.Category.findOrCreate = function($list, title) {
-    var categories = App.categories
-      , i = categories.length
-      , category;
+    var i = categories.length;
     while (i--) {
       if (categories[i].title === title) {
         return categories[i];
       }
     }
-    category = new App.Category($list, title);
-    categories.push(category);
-    return category;
+    return new App.Category($list, title);
   };
   
   
@@ -39,6 +36,7 @@
   
   p = App.Category.prototype;
   
+  // Return the HTML for this category in this list.
   p.listGroupHTML = function() {
     return '<div class="pdx911-category"><h2>' +
       this.title +
@@ -46,6 +44,24 @@
       config.listItemsGroupClass +
       '"></div></div>';
   };
+  
+  // A function for sorting categories alphabetically by title.
+  p.sorter = function(a, b) {
+    return a.title < b.title ? -1 : 1;
+  }
+
+  // Added this category's HTML to the list.
+  // The HTML is added in the appropriate alphabetical location.
+  p.renderIn = function($list) {
+    var i;
+    categories.sort(this.sorter);
+    i = categories.indexOf(this);
+    if (i === 0) {
+      this.$listGroup.prependTo($list);
+    } else {
+      this.$listGroup.insertAfter(categories[i - 1].$listGroup);
+    }
+  }
 
 
 
